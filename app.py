3f6243383e99786f9c07d51f61e6aeac89d49c7a
ren_mcc <ren_mcc@foxmail.com>
2020-09-22 11:29:57 +0800

    更新app.py脚本

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-  
 import os
 import json
 import logging
@@ -19,7 +20,7 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s %(message)s')
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/API/runner', methods=['POST', 'GET'])
 def send():
     if request.method == 'POST':
         post_data = request.get_data()
@@ -42,56 +43,44 @@ def send_alert(data):
     timestamp = int(round(time.time() * 1000))
     url = 'https://oapi.dingtalk.com/robot/send?access_token=%s&timestamp=%d&sign=%s' % (token, timestamp, make_sign(timestamp, secret))
 
-    status = data['status']
-    alerts = data['alerts']
-    alert_name = alerts[0]['labels']['alertname']
-
-    def _mark_item(alert):
-        labels = alert['labels']
-        annotations = "> "
-        for k, v in alert['annotations'].items():
-            annotations += "{0}: {1}\n".format(k, v)
-        if 'job' in labels:
-            mark_item = "\n> job: " + labels['job'] + '\n\n' + annotations + '\n'
+    for data in data['alerts']:
+        title = data["labels"]["severity"]
+        if data["status"] == "resolved":
+            stat = '# <font color=#006600>recovery</font>\n'
+        elif data["labels"]["severity"] == 'warning':
+            stat = '# <font color=#FF9933>%s</font>\n' % data["labels"]["severity"]
         else:
-            mark_item = annotations + '\n'
-        return mark_item
-
-    if status == 'resolved':  # 告警恢复
-        send_data = {
-            "msgtype": "text",
-            "text": {
-                "content": "报警 %s 已恢复" % alert_name
-            }
-        }
-    else:
-        title = '%s 有 %d 条新的报警' % (alert_name, len(alerts))
-        external_url = alerts[0]['generatorURL']
-        prometheus_url = os.getenv('PROME_URL')
-        if prometheus_url:
-            res = urlparse(external_url)
-            external_url = external_url.replace(res.netloc, prometheus_url)
+            stat = '# <font color=#CC0000>%s</font>\n' % data["labels"]["severity"]
+        instance = '- 实例: %s\n' % data["labels"]["instance"] if data["labels"].get("instance") else ""
+        alertname = '- 事件: %s\n' % data["labels"]["alertname"]
+        summary = '- 描述: %s\n' % data["annotations"]["summary"]
+        description = '- 项目: %s\n' % data["annotations"]["description"]
+        startsAt = '- 时间: %s\n' % data["startsAt"].split(".")[0].replace('T', ' ')
+        recive_data = stat + alertname + instance + description + summary + startsAt
         send_data = {
             "msgtype": "markdown",
             "markdown": {
-                "title": title,
-                "text": title + "\n" + "![](https://bxdc-static.oss-cn-beijing.aliyuncs.com/images/prometheus-recording-rules.png)\n" + _mark_item(alerts[0]) + "\n" + "[点击查看完整信息](" + external_url + ")\n"
+            "title":title,
+            "text":recive_data
+            },
+            "at":{
+            "atMobiles":[
+            ],
+            "isAtAll":True
             }
         }
-
-    req = requests.post(url, json=send_data)
-    result = req.json()
-    if result['errcode'] != 0:
-        app.logger.error('notify dingtalk error: %s' % result['errcode'])
-
+        req = requests.post(url, json=send_data)
+        result = req.json()
+        if result['errcode'] != 0:
+            app.logger.error(result)
 
 def make_sign(timestamp, secret):
     """新版钉钉更新了安全策略，这里我们采用签名的方式进行安全认证
     https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
     """
-    secret_enc = bytes(secret, 'utf-8')
+    secret_enc = secret.encode('utf-8')
     string_to_sign = '{}\n{}'.format(timestamp, secret)
-    string_to_sign_enc = bytes(string_to_sign, 'utf-8')
+    string_to_sign_enc = string_to_sign.encode('utf-8')
     hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
     sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
     return sign
